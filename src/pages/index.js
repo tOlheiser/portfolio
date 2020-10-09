e15580b429d46a1711dd0217f7810bcaf08df798
tOlheiser <tolheiser@outlook.com>
2020-10-09 11:50:43 -0600

    getting styled components set up

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
