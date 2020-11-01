8a0a15b086ecbdca435d1986a2bf9ceba4c85179
tOlheiser <tolheiser@outlook.com>
2020-11-01 12:06:48 -0600

    Added a theme file with initial styles

---
+++ src/styles/theme.js
@@ -0,0 +1,9 @@
+export const theme = {
+    primaryDark: '#2C2A3C',
+    primaryLight: '#CCD6F6',
+    primaryHover: '#343078',
+    mobile: '576px',
+    main: "#64ffDA",
+    primaryFont: "'Source Sans Pro', sans-serif",
+    monoFont: "'Source Code Pro', monospace"
+}
