c7877e522e0e3bf8a116187e6280a06fcbff0b31
tOlheiser <tolheiser@outlook.com>
2020-10-31 18:26:54 -0600

    Added a hook for hamMenu usability

---
+++ src/hooks.js
@@ -0,0 +1,21 @@
+import {useEffect} from 'react';
+
+/*  Purpose: Close the burger menu when user clicks outside of it.
+    When a user clicks, we need to check whether the clicked element is our current element (in this case, the div element that wraps the burger and menu components). If so, do nothing. Otherwise, call a function.*/
+
+export const useOnClickOutside = (ref, handler) => {
+  useEffect(() => {
+    const listener = event => {
+      if (!ref.current || ref.current.contains(event.target)) {
+        return;
+      }
+      handler(event);
+    };
+    document.addEventListener('mousedown', listener);
+    return () => {
+      document.removeEventListener('mousedown', listener);
+    };
+  },
+  [ref, handler],
+  );
+};
