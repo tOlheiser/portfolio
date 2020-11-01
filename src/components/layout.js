0c3994f2d93120205d98566f133ddca5cdeb6caa
tOlheiser <tolheiser@outlook.com>
2020-11-01 12:39:38 -0600

    Added imports and corresponding components

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,16 +9,21 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 import Header from "./header"
+import Email from "./email"
+import Social from "./social"
 import Hero from "./hero"
 import Work from "./work"
 import About from "./about"
 import Skills from "./skills"
 import Contact from "./contact"
+import {GlobalStyles} from '../styles/globalStyles'
 
 import "./layout.css"
 import style from "../pages/layout.module.css"
+import { ThemeProvider } from "styled-components"
+import { theme } from '../styles/theme'
 
-const Layout = ({ children }) => {
+const Layout = () => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -30,23 +35,30 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <div>
-    {/*Page Section*/}
-      <div className={style.landingPageContainer}>
-        <Header siteTitle={"Tanner Olheiser"} /> {/* data.site.siteMetadata.title*/}
-        {/*<Hero />*/}      
+    
+      <div>
+        <ThemeProvider theme={theme}>
+          <GlobalStyles/>
+          {/*Page Section*/}
+          <div className={style.landingPageContainer}>
+            <Header siteTitle={"Tanner Olheiser"} /> {/* data.site.siteMetadata.title*/}
+            <Email/>
+            <Social/>
+            {/*<Hero />*/}      
+          </div>
+          {/*
+          <div className={`${style.pageContainer} ${style.center}`}>
+            <Work />
+          </div> 
+          
+          <div className={`${style.pageContainer} ${style.center}`}>
+            <About />
+            <Skills />
+            <Contact />
+          </div> */}
+        </ThemeProvider>
       </div>
-      {/*
-      <div className={`${style.pageContainer} ${style.center}`}>
-        <Work />
-      </div> 
-      
-      <div className={`${style.pageContainer} ${style.center}`}>
-        <About />
-        <Skills />
-        <Contact />
-      </div> */}
-    </div>
+    
   )
 }
 
@@ -55,3 +67,16 @@ Layout.propTypes = {
 }
 
 export default Layout
+
+/* Her setup
+<StyledContent>
+  <Nav isHome={isHome} />
+  <Social isHome={isHome} />
+  <Email isHome={isHome} />
+
+  <div id="content">
+    {children}
+    <Footer />
+  </div>
+</StyledContent>
+*/
