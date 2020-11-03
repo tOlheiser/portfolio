3eeae9a2ba0290a32b7d3f0f1411c0a00da46750
tOlheiser <tolheiser@outlook.com>
2020-11-03 10:43:27 -0600

    Organized and added styles

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -1,9 +1,37 @@
 import { createGlobalStyle } from 'styled-components';
 
+// ! don't forget to setup reset styles
+
 export const GlobalStyles = createGlobalStyle`
   a {
     text-decoration: none;
     color: ${props => props.theme.primaryLight};
+  }
+  body {
+    background-color: #2C2A3C;
+    /* Making the body's position relative & declaring overflow as hidden 
+    will prevent a horizontal scrollbar from appearing when using translateX*/
+    position: relative;
+    overflow: hidden;
+  }
+
+/* These class name styles DO work.*/
+  .landingPageContainer {
+      height: 100vh;
+      background-color: #2C2A3C;
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-start;
+  }
+
+  .pageContainer { 
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+  }
+
+  .center {
+      align-items: center;
   }
 `
 // Set up font families
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,9 +1,20 @@
 export const theme = {
-    primaryDark: '#2C2A3C',
-    primaryLight: '#CCD6F6',
-    primaryHover: '#343078',
-    mobile: '576px',
-    main: "#64ffDA",
-    primaryFont: "'Source Sans Pro', sans-serif",
-    monoFont: "'Source Code Pro', monospace"
+  // Breakpoints
+  mobile: '576px',
+  // Colours
+  primaryDark: '#2C2A3C',
+  primaryLight: '#CCD6F6',
+  primaryHover: '#343078',
+  main: "#64ffDA",
+
+  // Font Related
+  primaryFont: "'Source Sans Pro', sans-serif",
+  monoFont: "'Source Code Pro', monospace",
+  // convert to em then implement.
+  xxs: '13px',
+  xs: '14px',
+  s: '16px',
+  m: '20px',
+  xl: '70px',
+  xxl: '80px'
 }
