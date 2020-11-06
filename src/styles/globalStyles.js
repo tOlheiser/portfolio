8b75b93ba179fe0416755b0d5af704b27799a7f3
tOlheiser <tolheiser@outlook.com>
2020-11-05 21:18:22 -0600

    reset styles + overflow-x change

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -1,18 +1,48 @@
 import { createGlobalStyle } from 'styled-components';
 
-// ! don't forget to setup reset styles
-
 export const GlobalStyles = createGlobalStyle`
+
+  /*     CSS RESET
+  ------------- */
+  html {
+    box-sizing: border-box;
+    /*font-size: 16px;    Decalre sizes in rem's, which is based off this. */
+  }
+
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+
+  body, h1, h2, h3, h4, h5, h6, p, ol, ul {
+    margin: 0;
+    padding: 0;
+    /* font-weight: normal;   Optional. */
+  }
+
+  ol, ul {
+    list-style: none;
+  }
+
+  /*  This makes images responsive by default 
+  img {
+    max-width: 100%;
+    height: auto;
+  }*/
+
+  /* END CSS RESET
+  ------------- */
+
   a {
     text-decoration: none;
     color: ${props => props.theme.primaryLight};
   }
+
   body {
     background-color: #2C2A3C;
     /* Making the body's position relative & declaring overflow as hidden 
     will prevent a horizontal scrollbar from appearing when using translateX*/
     position: relative;
-    overflow: hidden;
+    overflow-x: hidden; /* Allow overflow vertically but not horizontally. */
   }
 
 /* These class name styles DO work.*/
@@ -26,12 +56,11 @@ export const GlobalStyles = createGlobalStyle`
 
   .pageContainer { 
       display: flex;
-      flex-direction: column;
       width: 100%;
+      justify-content: center;
   }
 
   .center {
       align-items: center;
   }
 `
-// Set up font families
