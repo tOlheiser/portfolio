31090ece0a2f60b70e1b1a5502d45b62ca1c75d5
tOlheiser <tolheiser@outlook.com>
2020-11-05 21:16:18 -0600

    Cleaned up comments

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,98 +1,39 @@
 //import { Link } from "gatsby"
 import React from "react";
+import styled from "styled-components";
+import StyledButton from "./button";
+import { StyledH1, StyledH3, StyledP } from "../styles/contentTags"; 
 
-/*
-.heroDiv {
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    width: 960px;
-    max-width: 960px;
-    margin: 0 3em 9em 3em;
-    align-self: center;
-    justify-content: center;
-    border: 2px solid green;
-}
+const StyledHeroDiv = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  /*width: 960px;*/
+  max-width: 960px;
+  margin: 0 3em 9em 3em;
+  align-self: center;
+  justify-content: center;
+`
 
-.subheading {
-    font-family: 'Source Code Pro', sans-serif;
-    font-size: 16px;
-    line-height: 17.6px;
-    color: #64FFDA;
-}
+const StyledAccentH1 = styled(StyledH1)`
+  color: #8892B0;
+  padding-bottom: .3em;
+`
 
-.nameHeading {
-    font-family: 'Source Sans Pro', serif;
-    font-size: 80px;
-    line-height: 88px;
-    color: #CCD6F6;
-}
-
-.tagline {
-    font-family: 'Source Sans Pro', serif;
-    font-size: 80px;
-    line-height: 72px;
-    color: #8892B0;
-}
-
-.contentType {
-    font-family: 'Source Sans Pro', serif;
-    font-size: 20px;
-    line-height: 26px;
-    color: #8892B0;
-    max-width: 600px;
-}
-
-button {
-    border: 1px solid #64ffDA;
-    border-radius: .3em;
-    padding: 1rem 1.5rem;
-    margin: 0;
-    background: transparent;
-    color: #64ffDA;
-    line-height: 14px;
-    cursor: pointer;
-    text-align: center;
-    transition: background 250ms ease-in-out, 
-                transform 150ms ease;
-    -webkit-appearance: none;
-    -moz-appearance: none;
-}
-
-button:hover,
-button:focus {
-    background: rgba(100, 255, 218, .12);
-    
-}
-
-button:focus {
-    outline: none;
-    outline-offset: -4px;
-}
-
-button:active {
-    transform: scale(0.99);
-}
-
-.button {
-    font-family: 'Source Code Pro', sans-serif;
-    font-size: 14px;
-    line-height: 14px;
-    align-self: flex-start;
-    margin-top: 1.5em;
-}
-Source Code Pro or Cousine
-PT Mono looks awful as my nav link font.
-PT Mono: Feels a little fuzzy. A lighter weight might be nicer. */
+const StyledLgButton = styled(StyledButton)`
+  padding: 1.1rem 1.3rem;
+  font-size: 15px;
+  line-height: 16px;
+`
 
 const Hero = () => (
-    <div>
-        <h2>Hi, my name is</h2>
-        <h1>Tanner Olheiser.</h1>
-        <h1>I build things for the web.</h1>
-        <p>I'm a fullstack developer based in Regina, SK specializing in building exceptional web applications using React, Express, Node, and MongoDB.</p>
-        <button>Get in Touch</button>
-    </div>
+  <StyledHeroDiv>
+    <StyledH3>Hi, my name is</StyledH3>
+    <StyledH1>Tanner Olheiser.</StyledH1>
+    <StyledAccentH1>I'm a fullstack developer.</StyledAccentH1>
+    <StyledP>I'm a fullstack developer based in Regina, SK specializing in building exceptional web applications using React, Express, Node, and MongoDB.</StyledP>
+    <StyledLgButton>Get in Touch</StyledLgButton>
+  </StyledHeroDiv>
 )
 
 export default Hero
