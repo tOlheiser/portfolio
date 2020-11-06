01a4c6cfcdc7efdfac1a31123bf757f9db077e4f
tOlheiser <tolheiser@outlook.com>
2020-11-05 21:16:37 -0600

    Added a file to store skill info

---
+++ src/content/skillContent.js
@@ -0,0 +1,64 @@
+const skills = [
+  {
+    iconBackground: "background color",
+    icon: "icon svg",
+    title: "example: 'Frontend'",
+    skills: [
+      {
+        category: "what type of skill is this?",
+        skill: "name of language/framework/tool"
+      },
+      {
+        category: "type of skill",
+        skill: "name of language"
+      }
+    ]
+  },
+  {
+    iconBackground: "background color",
+    icon: "icon svg",
+    title: "example: 'Frontend'",
+    skills: [
+      {
+        category: "what type of skill is this?",
+        skill: "name of language/framework/tool"
+      },
+      {
+        category: "type of skill",
+        skill: "name of language"
+      }
+    ]
+  },
+  {
+    iconBackground: "background color",
+    icon: "icon svg",
+    title: "example: 'Frontend'",
+    skills: [
+      {
+        category: "what type of skill is this?",
+        skill: "name of language/framework/tool"
+      },
+      {
+        category: "type of skill",
+        skill: "name of language"
+      }
+    ]
+  },
+  {
+    iconBackground: "background color",
+    icon: "icon svg",
+    title: "example: 'Frontend'",
+    skills: [
+      {
+        category: "what type of skill is this?",
+        skill: "name of language/framework/tool"
+      },
+      {
+        category: "type of skill",
+        skill: "name of language"
+      }
+    ]
+  }
+]
+
+export default skills;
