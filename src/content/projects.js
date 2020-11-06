dd57e6bd5f4f6114db2ca44cd915c68d59de182c
tOlheiser <tolheiser@outlook.com>
2020-11-05 21:15:02 -0600

    Added a file to store all my project info

---
+++ src/content/projects.js
@@ -0,0 +1,28 @@
+const projects = [
+  {
+    name: "Certifi",
+    //photo: "photo address",
+    description: "Phasellus nibh est, blandit nec consectetur ac, accumsan a ligula. Integer tortor ex, fermentum eu ullamcorper elementum, tincidunt vel felis. Vestibulum eu turpis consequat, convallis lacus venenatis, consequat augue.",
+    tags: ["React", "language 1", "tooling", "framework"],
+    sourceCode: "link to github page",
+    demo: "link to project demo"
+  },
+  {
+    name: "Accountability App",
+    //photo: "photo address",
+    description: "Phasellus nibh est, blandit nec consectetur ac, accumsan a ligula. Integer tortor ex, fermentum eu ullamcorper elementum, tincidunt vel felis. Vestibulum eu turpis consequat, convallis lacus venenatis, consequat augue. ",
+    tags: ["language 1", "tooling", "framework"],
+    sourceCode: "link to github page",
+    demo: "link to project demo"
+  },
+  {
+    name: "Hacker News",
+    //photo: "photo address",
+    description: "Phasellus nibh est, blandit nec consectetur ac, accumsan a ligula. Integer tortor ex, fermentum eu ullamcorper elementum, tincidunt vel felis. Vestibulum eu turpis consequat, convallis lacus venenatis, consequat augue.",
+    tags: ["language 1", "tooling", "framework"],
+    sourceCode: "link to github page",
+    demo: "link to project demo"
+  }
+];
+
+export default projects;
