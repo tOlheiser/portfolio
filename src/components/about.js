67a0ec96cfe5fd9602c41c9979c44ddcd1a4e3d8
tOlheiser <tolheiser@outlook.com>
2020-11-06 20:38:57 -0600

    Converted into styled components

--- src/components/about.js
+++ src/components/about.js
@@ -1,102 +1,48 @@
 import React from "react";
+import styled from "styled-components";
+import SectionHeading from "./sectionHeading";
+import { StyledP } from "../styles/contentTags";
 import PropTypes from 'prop-types';
 
-/*
-.sectionContainer {
-    display: flex;
-    width: 960px;
-    flex-direction: column;
-    justify-content: center;
-    align-items: flex-start;
-    border: 2px solid red;
-}
+const StyledSectionDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 80%;
+`;
 
-.contentContainer {
-    padding-left: 3em;
-    padding-right: 3em;
-}
+const StyledRowDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-top: 60px;
+  margin-bottom: 60px;
+`;
 
-.skillSection {
-    display: flex;
-    flex-direction: row;
-    flex-wrap: nowrap;
-    border: 2px solid blue;
-    max-width: 960px;
-    width: 960px;
-    justify-content: space-between;
-}
+const PhotoDiv = styled.div`
+  width: 250px;
+  height: 300px;
+  max-width: 250px;
+  max-height: 300px;
+  background-color: ${props => props.theme.main};
+`;
 
-.skillCard {
-    display: flex;
-    padding: 1em;
-}
-
-.skillCard:first-of-type {
-    padding-left: 0;
-}
-
-.skillCard:last-of-type {
-    padding-right: 0;
-}
-
-.skillIcon {
-    width: 270px;
-    height: 340px;
-    background-color: #CCD6F6;
-    border: 2px solid yellow;
-}
-
-.skillIcon:first-child {
-    padding-left: 0;
-}
-
-.skillContent {
-
-}
-
-.sectionHeading {
-    font-family: 'Source Sans Pro', sans-serif;
-    font-size: 70px;
-    line-height: 70px;
-    color: #CCD6F6;
-}
-
-.subHeading {
-    font-family: 'Source Code Pro', monospace;
-    font-size: 16px;
-    line-height: 17.6px;
-    color: #64FFDA;
-}
-
-p {
-    font-family: 'Source Sans Pro', sans-serif;
-    font-size: 20px;
-    line-height: 26px;
-    color: #8892B0;
-}
-*/
+const P = styled(StyledP)`
+  max-width: 500px;
+  margin-right: 40px;
+`;
 
 const About = () => ( 
-    <div> {/* Section Container - Max width and c
-    entered in 100vh container*/}
-        <div> {/* Section Heading */}
-            <h2>Introductions</h2>
-            <h1><span>About</span> Me</h1> {/* Accent color on 'About' */}
-        </div>
-        <div>{/* Section Content */}
-            <div>{/* Project Card Container */}
-                <p>Hi there! My name's Tanner, and I'm an aspiring fullstack developer currently living in Regina, SK.
+  <StyledSectionDiv>
+    <SectionHeading heading="About Me" subheading="Introductions"/>
+    <StyledRowDiv>
+      <P>Hi there! My name's Tanner, and I'm an aspiring fullstack developer currently living in Regina, SK.
 
-                <br/><br/>What I really love about fullstack development is that I have the skills to bring ideas & dreams into reality. It brings me pride to know how everything works, from the design process, to how the front-end brings your pages to life, and how the back-end delivers those pages to the user.
+      <br/><br/>What I really love about fullstack development is that I have the skills to bring ideas & dreams into reality. It brings me pride to know how everything works, from the design process, to how the front-end brings your pages to life, and how the back-end delivers those pages to the user.
 
-                <br/><br/>My ambition is to continue to expand on my skillset and apply it in an environment where I'm doing interesting, meaningful work on a daily basis.</p>
-            </div>
+      <br/><br/>My ambition is to continue to expand on my skillset and apply it in an environment where I'm doing interesting, meaningful work on a daily basis.</P>
+      <PhotoDiv/>
+    </StyledRowDiv>
+  </StyledSectionDiv>
+);
 
-            <div>{/* Project Card Container */}
-                {/* Image here*/}
-            </div>
-        </div>
-    </div>
-)
-
-export default About
+export default About;
