561175051bcc5190d847d83d38bb8466f7802a21
tOlheiser <tolheiser@outlook.com>
2020-11-09 11:19:09 -0600

    Small modifications to numerous components.

--- src/components/hero.js
+++ src/components/hero.js
@@ -2,22 +2,25 @@
 import React from "react";
 import styled from "styled-components";
 import StyledButton from "./button";
-import { StyledH1, StyledH3, StyledP } from "../styles/contentTags"; 
+import { StyledH1, Subheading, StyledP } from "../styles/contentTags"; 
 
-const StyledHeroDiv = styled.div`
+const Container = styled.section`
   height: 100%;
   display: flex;
   flex-direction: column;
-  /*width: 960px;*/
-  max-width: 960px;
-  margin: 0 3em 9em 3em;
+  margin: 0 2.3rem 7rem 2.3rem;
   align-self: center;
   justify-content: center;
+  padding: 0 4rem;
+  border: 1px solid red;
+
+  @media (max-width: 900px) {
+    padding: 0 2.3rem;
+  }
 `
 
-const StyledAccentH1 = styled(StyledH1)`
+const Accent = styled.span`
   color: #8892B0;
-  padding-bottom: .3em;
 `
 
 const StyledLgButton = styled(StyledButton)`
@@ -27,13 +30,13 @@ const StyledLgButton = styled(StyledButton)`
 `
 
 const Hero = () => (
-  <StyledHeroDiv>
-    <StyledH3>Hi, my name is</StyledH3>
-    <StyledH1>Tanner Olheiser.</StyledH1>
-    <StyledAccentH1>I'm a fullstack developer.</StyledAccentH1>
+  <Container>
+    <Subheading>Hi, my name is</Subheading>
+    <StyledH1>Tanner Olheiser.<br/>
+    <Accent>I'm a full-stack web developer.</Accent></StyledH1>
     <StyledP>I'm a fullstack developer based in Regina, SK specializing in building exceptional web applications using React, Express, Node, and MongoDB.</StyledP>
-    <StyledLgButton>Get in Touch</StyledLgButton>
-  </StyledHeroDiv>
+    <StyledLgButton>See Projects</StyledLgButton>
+  </Container>
 )
 
 export default Hero
