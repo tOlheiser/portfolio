845f2a7b84695217a9191a229ff6ae1e27b64a1a
tOlheiser <tolheiser@outlook.com>
2020-11-09 11:24:53 -0600

    Gave rem values to my font-size variables.

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -11,13 +11,17 @@ export const theme = {
   // Font Related
   primaryFont: "'Source Sans Pro', sans-serif",
   monoFont: "'Source Code Pro', monospace", // Try to get a lighter font-weight
-  // convert to em then implement.
-  xxs: '13px',
-  xs: '14px',
-  s: '16px',
-  m: '20px',
-  xl: '70px',
-  xxl: '80px'
+
+  // Font sizes
+  xxs: '.8rem',
+  xs: '.9rem', // '14px',
+  s: '1rem',
+  subh: 'clamp(.85rem, 1.5vw, 1rem)',
+  m: '1.2rem',
+  l: 'clamp(1.7rem, 3vw, 2rem)', //'1.8rem',
+  xl: 'clamp(2.6rem, 5vw, 4rem)',//'4rem',
+  title: 'clamp(3rem, 6vw, 5rem)'
+  // get a font size set up for when the sidebar goes away
 }
 /*
 Source Code Pro or Cousine
