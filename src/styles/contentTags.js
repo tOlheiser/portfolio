0a6ffb730d7e9c3f7ec4d7dcc36652e868f0c6e8
tOlheiser <tolheiser@outlook.com>
2020-11-09 11:26:06 -0600

    Modified content tags to use theme values

--- src/styles/contentTags.js
+++ src/styles/contentTags.js
@@ -1,48 +1,43 @@
 import styled from "styled-components";
 
 export const StyledH1 = styled.h1` 
-  font-family: 'Source Sans Pro', serif;
-  font-size: 80px;
-  line-height: 88px;
-  color: #CCD6F6; 
+  font-family: ${props => props.theme.primaryFont};
+  font-size: ${props => props.theme.title};
+  color: ${props => props.theme.primaryLight};
+  line-height: clamp(3.4rem, 6vw, 5rem); /* 5rem */
 `
 
 export const StyledH2 = styled.h2` 
-  font-family: 'Source Sans Pro', serif;
-  font-size: 65px;
-  line-height: 68px;
-  color: #CCD6F6; 
+  font-family: ${props => props.theme.primaryFont};
+  font-size: ${props => props.theme.xl};
+  color: ${props => props.theme.primaryLight}; 
 ` 
 
-
 export const StyledH3 = styled.h3` 
-  font-family: 'Source Code Pro', sans-serif;
-  font-size: 16px;
-  line-height: 17.6px;
-  color: #64FFDA;
-  padding-bottom: 1em;
-  margin-top: 3em;
+  font-family: ${props => props.theme.primaryFont};
+  font-size: ${props => props.theme.l};
+  color: ${props => props.theme.primaryLight};
 `
 
-/* export const StyledH4 = styled.h4` 
-
+export const Subheading = styled.p`
+  font-family: ${props => props.theme.monoFont};
+  font-size: ${props => props.theme.subh};
+  color: ${props => props.theme.main};
+  font-weight: 600;
 `
-*/
 
 export const StyledP = styled.p` 
-  font-family: 'Source Sans Pro', serif;
-  font-size: 20px;
-  line-height: 26px;
-  color: #8892B0;
-  max-width: 600px; 
-  padding-bottom: 2.3em;
+  font-family: ${props => props.theme.primaryFont};
+  font-size: ${props => props.theme.m};;
+  color: ${props => props.theme.accentLight};
+  max-width: 65ch;
+  line-height: 1.9rem;
 `
 
 export const StyledTags = styled.p`
-  font-family: 'Source Code Pro', sans-serif;
-  font-size: 14px;
+  font-family: ${props => props.theme.monoFont};
+  font-size: ${props => props.theme.xs};
   color: ${props => props.theme.main};
   display: inline-block;
   padding-right: 1em;
-  
 `
