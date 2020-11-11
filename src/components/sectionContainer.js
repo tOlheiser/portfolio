c195b01489630f6e9208b5e181dc72b9fb8071a7
tOlheiser <tolheiser@outlook.com>
2020-11-10 23:15:40 -0600

    Removed width: 100vw because it caused a bug

--- src/components/sectionContainer.js
+++ src/components/sectionContainer.js
@@ -4,7 +4,6 @@ import styled from 'styled-components';
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  width: 100vw;
   justify-content: center;
   align-items: center;
 `
