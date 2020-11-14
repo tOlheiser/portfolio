7deebf410510d20ce09bc2a6a25a5ecee1b3b0d4
tOlheiser <tolheiser@outlook.com>
2020-11-13 22:07:08 -0600

    Made subheading an h4 for convenience

--- src/styles/contentTags.js
+++ src/styles/contentTags.js
@@ -1,10 +1,12 @@
 import styled from "styled-components";
 
+/* Add <a/> tags for my nav/menu links */
+
 export const StyledH1 = styled.h1` 
   font-family: ${props => props.theme.primaryFont};
   font-size: ${props => props.theme.title};
   color: ${props => props.theme.primaryLight};
-  line-height: clamp(3.4rem, 6vw, 5rem); /* 5rem */
+  line-height: clamp(3.4rem, 6vw, 5rem);
 `
 
 export const StyledH2 = styled.h2` 
@@ -19,11 +21,12 @@ export const StyledH3 = styled.h3`
   color: ${props => props.theme.primaryLight};
 `
 
-export const Subheading = styled.p`
+export const Subheading = styled.h4`
   font-family: ${props => props.theme.monoFont};
   font-size: ${props => props.theme.subh};
   color: ${props => props.theme.main};
   font-weight: 600;
+  /* Need to define a padding that takes in the padding size as props */
 `
 
 export const StyledP = styled.p` 
@@ -32,6 +35,7 @@ export const StyledP = styled.p`
   color: ${props => props.theme.accentLight};
   max-width: 65ch;
   line-height: 1.9rem;
+  /* Consider defining a padding that takes in the padding size as props */
 `
 
 export const StyledTags = styled.p`
