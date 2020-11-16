a5f5dc9355a18d141bbca01c2480361f68ebf0e6
tOlheiser <tolheiser@outlook.com>
2020-11-15 19:56:54 -0600

    Added project descriptions.

--- src/content/projects.js
+++ src/content/projects.js
@@ -2,34 +2,38 @@ const projects = [
   {
     name: "Certifi",
     //photo: "photo address",
-    description: "Phasellus nibh est, blandit nec consectetur ac, accumsan a ligula. Integer tortor ex, fermentum eu ullamcorper elementum, tincidunt vel felis. Vestibulum eu turpis consequat, convallis lacus venenatis, consequat augue.",
-    tags: ["React", "language 1", "tooling", "framework"],
+    description: "Having worked in the oil & gas industry, keeping track of your safety tickets is a hassle. Forgot to bring your tickets to the lease? You don't get to work. With this app you can add electronic versions of you tickets, track expiring tickets, and compare prices of safety courses near you.",
+    tagline: "Manage your certifications.",
+    tags: ["MongoDB", "Express", "Node", "React", "Web Scraping"],
     sourceCode: "link to github page",
     demo: "link to project demo"
   },
   {
-    name: "Accountability App",
+    name: "Hacker News Clone",
     //photo: "photo address",
-    description: "Phasellus nibh est, blandit nec consectetur ac, accumsan a ligula. Integer tortor ex, fermentum eu ullamcorper elementum, tincidunt vel felis. Vestibulum eu turpis consequat, convallis lacus venenatis, consequat augue. ",
-    tags: ["language 1", "tooling", "framework"],
-    sourceCode: "link to github page",
-    demo: "link to project demo"
+    description: "Completed as part of Tyler McGinnis' React course, which built on my React knowledge by introducing concepts such as Higher Order Components, Render Props, Context, and React Router. This was also my introduction to fetching data from an API using asynchronous programming.",
+    tagline: "View top posts, new posts, and user history in.",
+    tags: ["React", "Async", "Hacker News API", "React Router"],
+    sourceCode: "https://github.com/tOlheiser/react-hackerNews-clone",
+    demo: "https://olheiser-react-hackernews.netlify.app/"
   },
   {
-    name: "Hacker News",
+    name: "To Do List",
     //photo: "photo address",
-    description: "Phasellus nibh est, blandit nec consectetur ac, accumsan a ligula. Integer tortor ex, fermentum eu ullamcorper elementum, tincidunt vel felis. Vestibulum eu turpis consequat, convallis lacus venenatis, consequat augue.",
-    tags: ["language 1", "tooling", "framework"],
-    sourceCode: "link to github page",
-    demo: "link to project demo"
+    description: "My take on the classic 'To-do List' project. In this project I got my feet wet rendering lists and conditionally displaying list data. Managing state was also far more complex here than in a calculator.",
+    tags: ["React"],
+    tagline: "Add, track, and delete tasks.",
+    sourceCode: "https://github.com/tOlheiser/react-toDo-list",
+    demo: "https://olheiser-react-todolist.netlify.app/"
   },
   {
-    name: "Friend Maker",
+    name: "Calculator",
     //photo: "photo address",
-    description: "Phasellus nibh est, blandit nec consectetur ac, accumsan a ligula. Integer tortor ex, fermentum eu ullamcorper elementum, tincidunt vel felis. Vestibulum eu turpis consequat, convallis lacus venenatis, consequat augue.",
-    tags: ["language 1", "tooling", "framework"],
-    sourceCode: "link to github page",
-    demo: "link to project demo"
+    description: "First React app built after reading the React documentation. It was a great exercise in learning the component lifecycle and how to pass data between components.",
+    tags: ["React"],
+    tagline: "A basic, stylish calculator.",
+    sourceCode: "https://github.com/tOlheiser/react-calculator",
+    demo: "https://olheiser-react-calculator.netlify.app/"
   }
 ];
 
